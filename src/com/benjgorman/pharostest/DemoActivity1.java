745461e162d08a7abebf57e6ac46aa7fb68183a6
Daniel Herron <norrehlienad90@hotmail.com>
2011-09-20 15:10:20 +0100

    Added some spacing - testing#

--- src/com/benjgorman/pharostest/DemoActivity1.java
+++ src/com/benjgorman/pharostest/DemoActivity1.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 public class DemoActivity1 extends Activity{
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		
+		
+		super.onCreate(savedInstanceState);
         setContentView(R.layout.demo_activity1);
     }
 }
