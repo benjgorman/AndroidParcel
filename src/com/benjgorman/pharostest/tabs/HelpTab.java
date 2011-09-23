2c34f1a879cd893b2a9ef09925dc1b05c53a3a27
Daniel Herron <norrehlienad90@hotmail.com>
2011-09-23 15:29:39 +0100

    Help email activity

--- src/com/benjgorman/pharostest/tabs/HelpTab.java
+++ src/com/benjgorman/pharostest/tabs/HelpTab.java
@@ -4,7 +4,11 @@ import com.benjgorman.pharostest.R;
 import com.benjgorman.pharostest.R.layout;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HelpTab extends Activity{
 	@Override
@@ -13,6 +17,32 @@ public class HelpTab extends Activity{
 		
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+        
+        Button button = (Button)findViewById(R.id.btn_email);
+        button.setOnClickListener(
+           	new OnClickListener()
+            	{
+    				public void onClick(View v) {
+    					Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+    					String aEmailList[] = { "norrehlienad90@hotmail.com" };
+    					String aEmailCCList[] = { "" };
+    					String aEmailBCCList[] = { "" };
+
+    					emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);
+    					emailIntent.putExtra(android.content.Intent.EXTRA_CC, aEmailCCList);
+    					emailIntent.putExtra(android.content.Intent.EXTRA_BCC, aEmailBCCList);
+
+    					emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Additional Question");
+
+    					emailIntent.setType("plain/text");
+    					emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+
+    					startActivity(emailIntent);
+    				}	
+           	}
+         );
+        
     }
 }
 
