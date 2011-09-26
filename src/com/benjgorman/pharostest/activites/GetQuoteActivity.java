93b7402f8abdf5efbeed73948416a291e731fd64
Daniel Herron <norrehlienad90@hotmail.com>
2011-09-26 17:54:21 +0100

    Added alert dialog for GetQuote

--- src/com/benjgorman/pharostest/activites/GetQuoteActivity.java
+++ src/com/benjgorman/pharostest/activites/GetQuoteActivity.java
@@ -1,12 +1,14 @@
 package com.benjgorman.pharostest.activites;
 
 import com.benjgorman.pharostest.R;
+import com.benjgorman.pharostest.SendParcel;
 import com.benjgorman.pharostest.R.array;
 import com.benjgorman.pharostest.R.id;
 import com.benjgorman.pharostest.R.layout;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +18,8 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.app.AlertDialog;
 
 public class GetQuoteActivity extends Activity{
 	Context context;
@@ -29,7 +33,7 @@ public class GetQuoteActivity extends Activity{
 		setContentView(R.layout.getquote);
         
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
-               
+        
         //Add a few countries to the spinner
        Spinner spinnerCountries = (Spinner) findViewById(R.id.spinner_collection_address);
        ArrayAdapter countryArrayAdapter = new ArrayAdapter(this,
@@ -37,27 +41,37 @@ public class GetQuoteActivity extends Activity{
                    new String[] { "UK", "Ireland", "Canada", "USA", "France", "Spain", "Germany", });
        spinnerCountries.setAdapter(countryArrayAdapter);
        
-       //Add a few countries to the spinner
+       //Add weight to the spinner
       Spinner spinnerWeight = (Spinner) findViewById(R.id.spinner_weight);
       ArrayAdapter weightArrayAdapter = new ArrayAdapter(this,
                   android.R.layout.simple_spinner_dropdown_item,
                   new String[] { "0.5kg", "1kg", "1.5kg", "2kg", "2.5kg", "3kg", "3.5kg", "4kg", });
       spinnerWeight.setAdapter(weightArrayAdapter);
         
-        
+      //Add services to the spinner
+     Spinner spinnerService = (Spinner) findViewById(R.id.spinner_service);
+     ArrayAdapter serviceArrayAdapter = new ArrayAdapter(this,
+                 android.R.layout.simple_spinner_dropdown_item,
+                 new String[] { "UPS: Standard", "UPS: Express", "UPS: Express Saver", "UPS: Expedited", "Yodel: Standard", "Yodel: Saturday Delivery", "Yodel: AM Delivery", "Yodel: PM Delivery", "Yodel: Evening Delivery", });
+     spinnerService.setAdapter(serviceArrayAdapter);
 
         
+     
         final Button button = (Button) findViewById(R.id.btn_quoteme);
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            public void onClick(View v) {          	          	
             	
-//            	Builder builder = new AlertDialog.Builder();
-//                builder.setTitle("Quote");
-//                builder.setMessage("Your quote is: ");
-//                builder.setPositiveButton("Send this package", null);
-//                builder.setNegativeButton("Cancel", null);
-//                builder.show();                
-
+            	new AlertDialog.Builder(GetQuoteActivity.this).setMessage("Your parcel will cost £34.75. Would you like to send it?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                	public void onClick(DialogInterface arg0, int arg1) {          			         
+//                		Intent intent = new Intent(context, SendParcel1.class);
+//            				context.startActivity(intent);
+                	}
+                })
+            	.setNeutralButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                arg0.dismiss();                                     }
+                        }).show();          
             }
         });
 
